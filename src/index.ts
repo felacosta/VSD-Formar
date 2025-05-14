78bc25192b8e46977dabf45ad710147726810c0a
felacosta <felipecp2022@gmail.com>
2025-05-14 00:04:28 +0000

    Criação de usuario

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,29 @@
 import express from "express";
+import User from "./models/User";
+import { sequelize } from "./config/database";
 
 const app = express();
+
 const port = 3000;
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.listen(port, () => console.log(`Listening at http://localhost:${port}`));
+
+main()
+
+async function main() {
+  if (require.main === module) {
+    await sequelize.sync({ force: true}).then(() => {
+      console.log("Database is synced");
+      app.listen(port, () =>
+      console.log('Listening at http://localhost:${port}'),
+    );
+    });
+  
+  // Criar usuário
+  const user = await User.create({ name: 'Alice', email: 'alice@email.com'});
+  console.log("Usuário criado:", user.toJSON());
+}
+}
